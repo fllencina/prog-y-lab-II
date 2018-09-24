323d0767d1e799e56570d5f6687ea3eee786c729
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-09-24 15:30:02 -0300

    Add files via upload

---
+++ Ejercicio 38/Ejercicio 38/Sobreescrito.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_38
+{
+    class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+        public Sobreescrito()
+        {
+
+        }
+        public override bool Equals(Object objeto)
+        {
+            if ((objeto == null) || !this.GetType().Equals(objeto.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+
+
+    }
+}
