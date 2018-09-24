9f433c0c093a056c1af5a990caa98d2f4349b73e
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-09-24 17:55:28 -0300

    error - no compila

---
+++ Ejercicio 39/Ejercicio 39/Sobreescrito.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_39
+{
+     class Sobreescrito
+    {
+        protected string miAtributo;
+        //constructor
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar abstractos";
+        }
+        //propiedad
+        public abstract string MiPropiedad(); 
+
+        //override
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+
+        public override bool Equals(Object objeto)
+        {
+            if ((objeto == null) || !this.GetType().Equals(objeto.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+
+
+}
+
