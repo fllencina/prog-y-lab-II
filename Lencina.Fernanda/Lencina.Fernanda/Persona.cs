2b4d4ed12401399c8e9070edc62a77e1dbf8ae54
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-10-11 18:58:31 -0300

    primer parcial aprobado

---
+++ Lencina.Fernanda/Lencina.Fernanda/Persona.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        private string apellido;
+        private int dni;
+        private int edad;
+        private string nombre;
+
+        #region "Propiedades"
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+        }
+        public int Dni
+        {
+            get
+            {
+                return this.dni;
+            }
+        }
+        public int Edad
+        {
+            get
+            {
+                return this.edad;
+            }
+        }
+        #endregion
+        #region "Constructor"
+        public Persona(string nombre,string apellido,int edad,int dni)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+            this.dni = dni;
+        }
+        #endregion
+        #region "Metodos"
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("\nNombre: {0} Apellido: {1} Edad: {2} Dni: {3}\n", Nombre, Apellido, Edad, Dni);
+            return sb.ToString();
+        }
+        public abstract bool ValidarAptitud();
+        #endregion
+    }
+}
---
+++ Lencina.Fernanda/VistaForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace VistaForm
+{
+    public partial class FormDT : Form
+    {
+        DirectorTecnico director;
+        public FormDT()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCrear_Click(object sender, EventArgs e)
+        {          
+            if(textBoxNombre.Text.Length>0 && textBoxApellido.Text.Length>0)
+            {
+                director = new DirectorTecnico(textBoxNombre.Text, textBoxApellido.Text, (short)numericUpDownEdad.Value, (short)numericUpDownDni.Value, (short)numericUpDownExperiencia.Value);
+                MessageBox.Show("Se ha creado el DT!");
+            }
+           
+        }
+
+        private void buttonValidar_Click(object sender, EventArgs e)
+        {
+            if (director == null)
+            {
+                MessageBox.Show("Aun no se ha creado el DT del formulario");
+                
+            }
+            else
+            {
+                if (director.ValidarAptitud())
+                {
+                    MessageBox.Show("El DT es Apto!");
+                }
+                if (!director.ValidarAptitud())
+                {
+                    MessageBox.Show("El DT no es Apto.");
+                }
+            }
+        }
+    }
+}
