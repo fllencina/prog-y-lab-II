8fac17766d8f17ee3ac8656ebd4ca840bb623916
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-10-17 22:11:55 -0300

    Add files via upload

---
+++ Ejercicio 47/Ejercicio 47/Equipo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_47
+{
+    abstract class Equipo
+    {
+        protected string nombre;
+        protected DateTime fechaCreacion;
+
+        public Equipo(string nombre,DateTime fecha)
+        {
+            this.nombre = nombre;
+            this.fechaCreacion = fecha;
+        }
+        public static bool operator == (Equipo e1, Equipo e2)
+        {
+            if(e1.nombre== e2.nombre && e1.fechaCreacion == e2.fechaCreacion)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public static bool operator !=(Equipo e1, Equipo e2)
+        {
+            return !(e1 == e2);
+        }
+
+        public string Ficha()
+        {
+            StringBuilder ficha = new StringBuilder();
+            ficha.AppendFormat("{0} fundado el {1}", this.nombre, this.fechaCreacion);
+            return ficha.ToString();
+        }
+    }
+}
---
+++ Ejercicio 47/Ejercicio 47/Torneo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_47
+{
+    class Torneo<T> where T : Equipo
+    {
+        List<T> equipos;
+        string nombre;
+
+        public Torneo(string nombre)
+        {
+            equipos = new List<T>();
+            this.nombre = nombre;
+        }
+        public static bool operator ==(Torneo<T> a, Equipo e)
+        {
+            foreach (Equipo e2 in a.equipos)
+            {
+                if (e == e2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Torneo<T> a, Equipo e)
+        {
+            return !(a == e);
+        }
+        public static Torneo<T> operator +(Torneo<T> a, T e)
+        {
+            if(a!=e)
+            {
+                a.equipos.Add(e);
+            }
+            return a;
+        }
+        public string Mostrar()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendFormat("Nombre {0} \n\t", this.nombre);
+            foreach(Equipo e in equipos)
+            {
+                mostrar.AppendFormat("{0}\n\t", e.Ficha());
+            }
+
+        }
+    }
+}
