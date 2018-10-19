cebcd8a61227336f25f9043f6bb2161b9c91754f
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-10-18 21:55:25 -0300

    Add files via upload

---
+++ Ejercicio 50/Ejercicio 50/GuardarTexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_50
+{
+     class GuardarTexto<T,V>:IGuardar<T,V>
+    {
+        public bool Guardar(T obj)
+        {
+            return true;
+        }
+
+        public V Leer()
+        {
+            string texto = "Texto Leído";
+            return (V)Convert.ChangeType(texto, typeof(V));
+        }
+    }
+}
---
+++ Ejercicio 50/Ejercicio 50/Serializar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_50
+{
+    class Serializar <S,V>: IGuardar<S,V>
+   {
+        public bool Guardar(S obj)
+        {
+            return true;
+        }
+
+        public V Leer()
+        {
+            string texto = "Objeto Leído";
+            return (V)Convert.ChangeType(texto, typeof(V));
+        }
+   }
+}
