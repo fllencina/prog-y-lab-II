606196c9cedc9c44a90255135f48030f4e6a2328
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-10-18 22:07:20 -0300

    Add files via upload

---
+++ Ejercicio 52/Ejercicio 52/Boligrafo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_52
+{
+    class Boligrafo
+    {
+        ConsoleColor colorTinta;
+        float tinta;
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return this.colorTinta;
+            }
+            set
+            {
+                this.colorTinta = value;
+            }
+        }
+        public float Tinta
+        {
+            get
+            {
+                return this.tinta;
+            }
+            set
+            {
+                this.tinta = value;
+            }
+        }
+        public bool Recargar(int unidades)
+        {
+            
+        }
+    }
+}
---
+++ Ejercicio 52/Ejercicio 52/Lapiz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_52
+{
+    class Lapiz
+    {
+        float tamanioMina;
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return this.Color;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public float UnidadesDeEscritura
+        {
+            get
+            {
+                return this.tamanioMina;
+            }
+            set
+            {
+                this.tamanioMina = value;
+            }
+        }
+        public Lapiz(int unidades)
+        {
+            this.UnidadesDeEscritura = unidades;
+        }
+        public bool Recargar(int unidades)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
