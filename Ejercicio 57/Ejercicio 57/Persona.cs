5a7a4ed01df8cc45b934ac7f14cf5e46ddf876e6
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-11-01 21:42:38 -0300

    Add files via upload

---
+++ Ejercicio 57/Ejercicio 57/Persona.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio_57
+{
+    [Serializable]
+    public class Persona
+    {
+         private string nombre;
+         private string apellido;
+
+        public Persona()
+        { }
+        public Persona(string nombre, string apellido)
+        {
+            Nombre = nombre;
+           Apellido = apellido;
+        }
+        //Me armo propiedades para poder serializarlo, solo es posible en artibutos publicos
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = value;
+            }
+        }
+        public static void Guardar(Persona p, string ruta)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(Persona));
+            XmlWriter writer = new XmlTextWriter(ruta, null);
+            ser.Serialize(writer, p);
+            writer.Close();
+        }
+        public static Persona Leer(string ruta)
+        {
+            Persona aux = new Persona(null, null);
+            XmlTextReader reader = new XmlTextReader(ruta);
+            XmlSerializer ser = new XmlSerializer(typeof(Persona));
+
+            aux = (Persona)ser.Deserialize(reader);
+
+            reader.Close();
+            return aux;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre Completo: {1},{0}",Nombre,Apellido);
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Ejercicio 57/Ejercicio 57/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio_57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona unaPersona= new Persona("Fernanda", "Lencina");
+            Persona OtraPersona = null;
+            try
+            {
+                Persona.Guardar(unaPersona, @"D:\VisualStudio\fernanda\prog-y-lab-II-master\Ejercicio 57\Archivo.xml");
+            }
+            catch (System.UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                OtraPersona = Persona.Leer(@"D:\VisualStudio\fernanda\prog-y-lab-II-master\Ejercicio 57\Archivo.xml");
+                Console.WriteLine(OtraPersona.ToString());
+            }
+            catch (System.UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //OtraPersona =Persona.Leer(@"D:\VisualStudio\fernanda\prog-y-lab-II-master\Ejercicio 57\Archivo.xml");
+
+            Console.ReadKey();
+        }
+
+    }
+   
+    
+}
