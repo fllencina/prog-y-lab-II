8a06c83c8dd81d22eb15761850a35f01fc8dcac5
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2018-11-11 18:02:38 -0300

    tengo paqueteDAO y excepciones

---
+++ empiezo tp4/CorreoTP4/Paquete.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreoTP4
+{
+    public enum EEstado
+    {
+        Ingresado,
+        EnViaje,
+        Entregado
+    }
+    public delegate void DelegadoEstado(object sender, EventArgs e);
+
+    public class Paquete
+    {
+        public string DireccionEntrega
+        {
+            get
+            {
+                
+            }
+        }
+
+
+    }
+}
---
+++ empiezo tp4/CorreoTP4/PaqueteDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace CorreoTP4
+{
+    public static class PaqueteDAO
+    {
+        static SqlConnection conexion;
+        static SqlCommand comando;
+        static SqlDataReader reader;
+
+        static PaqueteDAO()
+        {
+            conexion = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=correo-sp-2017;integrated Security=true");
+        }
+
+        public static bool Insertar(Paquete p)
+        {
+            comando = new SqlCommand();
+            bool retorno = false;
+            try
+            {
+                conexion.Open();
+                comando = new SqlCommand("INSERT INTO Paquetes(Id,DireccionEntrega,TrackingId,Alumno) VALUES ('" + p.Id + "','" + p.DireccionEntrega + "','"+p.TrackingId+ "','" + "Lencina.Fernanda" + "')", conexion);
+                comando.ExecuteNonQuery();
+                retorno = true;
+            }
+            catch (TrackingIdRepetidoException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conexion.State == System.Data.ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+            return retorno;
+        }
+
+    }
+}
