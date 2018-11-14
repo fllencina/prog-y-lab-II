387318bea54f310f9b7c40513992f888dddb2238
alumno <alumno>
2018-11-14 20:27:33 -0300

    Ejercicio 69 funciona ok

---
+++ Ejercicio 69 alternativo/Ejercicio 69 alternativo/FrmDatos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Ejercicio_69_alternativo
+{
+    public partial class FrmDatos : Form
+    {
+        public FrmDatos()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDatos_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void ActualizarNombre(string nombre)
+        {           
+                this.label1.Text = nombre;
+        }
+            
+    }
+}
+
---
+++ Ejercicio 69 alternativo/Ejercicio 69 alternativo/frmTestDelegados.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_69_alternativo
+{
+    public partial class frmTestDelegados : Form
+    {
+
+        public delegate void Delegado(string nombre);
+        public event Delegado EventoDelegado;
+        public frmTestDelegados()
+        {
+            InitializeComponent();
+        }
+
+        public  void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if(EventoDelegado!=null)
+            EventoDelegado.Invoke(textBox1.Text);
+        }
+    }
+}
