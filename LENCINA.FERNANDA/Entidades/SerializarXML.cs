29337440ed269c82359c376ba3e44dcb9e7c19d8
alumno <alumno>
2018-11-15 21:31:14 -0300

    Parcial me faltan los test unitarios

---
+++ LENCINA.FERNANDA/Entidades/SerializarXML.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Excepciones;
+namespace Entidades
+{
+    public class SerializarXML<T> : IArchivos<T>
+    {
+        public bool Guardar(string ruta, T objeto)
+        {
+            if (ruta != null && objeto != null)
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                XmlWriter writer = new XmlTextWriter(ruta, null);
+                try
+                {
+                    ser.Serialize(writer, objeto);
+                }
+                catch
+                {
+                    throw new ErrorArchivoException("No fue posible escribir el archivo");
+                }
+                writer.Close();
+                return true;
+            }
+            return false;
+        }
+        public T Leer (string ruta)
+        {
+            T aux=default(T);
+            if (ruta != null)
+            {
+                
+                XmlTextReader reader = new XmlTextReader(ruta);
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                try
+                {
+                    aux = (T)ser.Deserialize(reader);
+                }
+                catch
+                {
+                    throw new ErrorArchivoException("No fue posible leer el archivo");
+                }
+                reader.Close();
+            }          
+            return aux;           
+        }
+    }
+}
---
+++ LENCINA.FERNANDA/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ea1fe2a-b8d6-481c-8bc2-9a9e2f046ca9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
